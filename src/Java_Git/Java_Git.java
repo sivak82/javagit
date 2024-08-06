c890ea21f695d8f87b8ee052b69a51cf4c915fee
siva <sivakumarkongara3@gmail.com>
2024-08-06 17:00:47 -0400

    Commiting first project

---
+++ src/Java_Git/Java_Git.java
@@ -0,0 +1,12 @@
+package Java_Git;
+
+public class Java_Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("welcome to git integration learning");
+
+	}
+
+}
