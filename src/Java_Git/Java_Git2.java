ce8f5bb56a4f4858a9745187398370735371fae4
siva <sivakumarkongara3@gmail.com>
2024-08-07 10:34:21 -0400

    Second program adding

---
+++ src/Java_Git/Java_Git2.java
@@ -0,0 +1,12 @@
+package Java_Git;
+
+public class Java_Git2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("second program");
+
+	}
+
+}
